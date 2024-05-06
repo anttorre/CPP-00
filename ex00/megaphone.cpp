c2acff65f5fe981677e0fdb9c4df28bedbbaecb1
anttorre <125140017+anttorre@users.noreply.github.com>
2024-05-06 17:04:05 +0200

    Add files via upload

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anttorre <atormora@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 12:51:00 by anttorre          #+#    #+#             */
+/*   Updated: 2024/05/06 15:48:45 by anttorre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+			for (int j = 0; j < (int)strlen(argv[i]); j++)
+				std::cout << (char)toupper(argv[i][j]);
+		std::cout << "\n";
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
