ce98e78a198cf485d6b7c5a87eb0a963440caf74
Antonio Torres Mora <atormora@gmail.com>
2024-05-15 15:20:20 +0200

    Phonebook fixed

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,24 +6,23 @@
 /*   By: anttorre <atormora@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 13:15:25 by anttorre          #+#    #+#             */
-/*   Updated: 2024/05/15 15:08:15 by anttorre         ###   ########.fr       */
+/*   Updated: 2024/05/15 15:19:45 by anttorre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "PhoneBook.hpp"
 
-void	ft_leaks()
+/* void	ft_leaks()
 {
-	system("leaks -q main");
-}
+	system("leaks -q phonebook");
+} */
 
 int	main(void)
 {
 	PhoneBook	phonebook;
 	std::string	str;
 	
-	atexit(ft_leaks);
 	str = "";
 	while (str != "EXIT")
 	{
