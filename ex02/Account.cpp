9476ee394fd0ab228599da73efbd4584976cec03
Antonio Torres Mora <atormora@gmail.com>
2024-05-16 16:44:33 +0200

    ex02

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,10 +6,14 @@
 /*   By: anttorre <atormora@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 16:26:51 by anttorre          #+#    #+#             */
-/*   Updated: 2024/05/15 16:27:30 by anttorre         ###   ########.fr       */
+/*   Updated: 2024/05/16 12:46:55 by anttorre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
