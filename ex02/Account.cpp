bb1179cd6b8bf94a16e9ad5b3a39a6eabb1183d7
Antonio Torres Mora <anttorre@c2r4s3.42malaga.com>
2024-05-27 15:25:55 +0200

    ex02

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -17,3 +17,38 @@ int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+    this->_amount = initial_deposit;
+    this->_accountIndex = this->getNbAccounts();
+    this->_nbDeposits = 0;
+    this->_nbWithdrawals = 0;
+    this->_totalAmount += initial_deposit;
+    this->_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+    this->_nbAccounts++;
+}
+
+Account::~Account()
+{
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
+	Account::_nbAccounts--;
+}
+
+void Account::_displayTimestamp(void)
+{
+    time_t now;
+    now = time(NULL);
+
+    char timestamp[20];
+    strftime(timestamp, 20, "[%Y%m%d_%H%M%S] ", localtime(&now));
+
+    std::cout << timestamp;
+}
+
+int Account::getNbAccounts()
+{
+    return Account::_nbAccounts;
+}
