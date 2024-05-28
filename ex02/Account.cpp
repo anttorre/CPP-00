fee8a941f76399c7a1b44873ef5df38f2c865e35
Antonio Torres Mora <anttorre@c2r6s1.42malaga.com>
2024-05-28 16:20:23 +0200

    ex02

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -41,14 +41,71 @@ void Account::_displayTimestamp(void)
 {
     time_t now;
     now = time(NULL);
-
     char timestamp[20];
     strftime(timestamp, 20, "[%Y%m%d_%H%M%S] ", localtime(&now));
-
     std::cout << timestamp;
 }
 
 int Account::getNbAccounts()
 {
     return Account::_nbAccounts;
+}
+
+int Account::getTotalAmount()
+{
+    return Account::_totalAmount;
+}
+
+int Account::getNbDeposits()
+{
+    return Account::_totalNbDeposits;
+}
+
+int Account::getNbWithdrawals()
+{
+    return Account::_totalNbWithdrawals;
+}
+
+void    Account::displayAccountsInfos()
+{
+    Account::_displayTimestamp();
+    std::cout << "accounts:" << Account::getNbAccounts() << ";total:" << Account::getTotalAmount() << ";deposits:" << Account::getNbDeposits() << ";withdrawals:" << Account::getNbWithdrawals() << std::endl;
+}
+
+int	Account::checkAmount() const
+{
+	return this->_amount;
+}
+
+void	Account::displayStatus() const
+{
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+}
+
+void Account::makeDeposit(int deposit)
+{
+	this->_nbDeposits++;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex <<";p_amount:"<< this->_amount <<";deposit:" << deposit << ";amount:" << this->_amount + deposit << ";nb_deposits:" << this->_nbDeposits << std::endl;
+	Account::_totalNbDeposits++;
+	this->_amount += deposit;
+	Account::_totalAmount += deposit;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawal:";
+	if (withdrawal > this->checkAmount())
+	{
+		std::cout << "refused" << std::endl;
+		return (false);
+	}
+	this->_nbWithdrawals++;
+	this->_totalNbWithdrawals++;
+	std::cout << withdrawal << ";amount:" << this->_amount - withdrawal << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+	this->_amount -= withdrawal;
+	this->_totalAmount -= withdrawal;
+	return (true);
 }
